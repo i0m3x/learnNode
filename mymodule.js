ea339c51db58f19e73859a37786012f017695f44
lexiconPDX <chaungemail@gmail.com>
2020-08-05 13:41:02 -0700

    'filtered

--- mymodule.js
+++ mymodule.js
@@ -1,25 +1,12 @@
 // Create two files named make-it-modular.js and mymodule.js.           
-                                                                     
-// This problem is the same as the previous (filtered-ls.js) but introduces the concept  
-// of modules. You will need to create two files to solve this.         
-                                                                     
-// Create a program that prints a list of files in a given directory,   
-// filtered by the extension of the files. The first argument is the    
-// directory name and the second argument is the extension filter.      
-// Print the list of files (one file per line) to the console. You must 
-// use asynchronous I/O.                                                
-                                                                     
-// You must write a module file (mymodule.js) to do most of the work.   
-// The module must export a single function that takes three arguments: 
-// the directory name, the filename extension string and your callback  
-// function, in that order. Don't alter the filename extension string   
-// in any way before passing it to your module.   
+                                                             
+// The module must export a single function that takes three 
 const fs = require("fs");
 function filteredList(dir, extensionEnding, cb){
-    fs.readdir(dir, (err, data =>  {
+    fs.readdir(dir, (err, data =>  { //reads contents of a dir
         const files = []
         if(err){
-            return cb
+            return cb(err)
         }
         //iterate through list of files
         for(let eachFile =0; i<data.length; eachFile++){
@@ -27,11 +14,12 @@ function filteredList(dir, extensionEnding, cb){
                 files.push(eachFile)
             }
         }
-    }                    
+        cb(err,files)
+    } )                   
    
-    
 }
-}
+
+
 // The module must export a single function that takes three arguments: 
 module.exports = filteredList
 // The callback function must be called using the idiomatic node(err,   
